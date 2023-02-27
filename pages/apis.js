88be8f6a84e8073799efa5629ef57a0aa6c9b6d7
markotomic <masheritto@gmail.com>
2023-02-27 01:45:55 +0100

    Changed API for web scraping...

--- pages/apis.js
+++ pages/apis.js
@@ -8,7 +8,7 @@ import Head from "next/head";
 
 const Apis = () => {
   const [loader, setLoader] = useState(true);
-  const url = "https://hexullu-apis-api-production.up.railway.app/hexullu";
+  const url = "https://green-factual-rain.glitch.me/hexullu";
   const [listOfApis, setListOfApis] = useState([]);
 
   useEffect(() => {
@@ -21,8 +21,14 @@ const Apis = () => {
   return (
     <div>
       <Head>
-        <title>Hexullu APIs - Best Free and Premium APIs for your WEB APP</title>
-        <meta property="og:title" content="Hexullu APIs - Best Free and Premium APIs for your WEB APP" key="title" />
+        <title>
+          Hexullu APIs - Best Free and Premium APIs for your WEB APP
+        </title>
+        <meta
+          property="og:title"
+          content="Hexullu APIs - Best Free and Premium APIs for your WEB APP"
+          key="title"
+        />
         <meta
           name="keywords"
           content="hexullu, hexullu apis, best apis, api hexullu, realtime api, live api, hexullu website, hexullu rapidapi"
@@ -39,17 +45,28 @@ const Apis = () => {
       <Navbar />
       <div className="Apis bg-[url('https://i.ibb.co/DL4Ft4D/hero-background.png')] bg-center bg-no-repeat bg-cover w-full bg-[#fff] flex justify-center items-center">
         <div className="ApisBox max-w-[1240px] min-h-[100vh] w-full py-[100px] px-4 flex flex-col items-center">
-          <h1 className="text-3xl font-bold text-[#333333]">Choose the API you want to use</h1>
+          <h1 className="text-3xl font-bold text-[#333333]">
+            Choose the API you want to use
+          </h1>
           <h2 className="text-sm font-[500] text-[#666666] mt-[10px] mb-[10px]">
-            All of APIs have a free plan with a fixed quota to test them at first
+            All of APIs have a free plan with a fixed quota to test them at
+            first
           </h2>
 
-          {loader && <ThreeDots color="#009688" height={80} width={80} timeout={30} />}
+          {loader && (
+            <ThreeDots color="#009688" height={80} width={80} timeout={30} />
+          )}
 
           <div className="ApisGrid grid grid-cols-1 md:grid-cols-4 gap-[20px]  gap-[50px] w-full  mt-[50px]">
             {listOfApis.map((Api) => {
               return (
-                <ApiCard key={Api.title} title={Api.title} description={Api.description} img={Api.img} url={Api.url} />
+                <ApiCard
+                  key={Api.title}
+                  title={Api.title}
+                  description={Api.description}
+                  img={Api.img}
+                  url={Api.url}
+                />
               );
             })}
           </div>
